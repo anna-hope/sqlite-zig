4bffda12b539506531338434de04ec560f686360
Anna Hope <anna.me@hey.com>
2024-07-26 12:23:35 -0500

    WIP parse insert statements

--- src/parser.zig
+++ src/parser.zig
@@ -1,4 +1,8 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const username_size = 32;
+const email_size = 255;
 
 pub const MetaCommandError = error{
     Unrecognized,
@@ -8,21 +12,49 @@ pub fn processMetaCommand(_: []u8) MetaCommandError!void {
     return error.Unrecognized;
 }
 
-pub const StatementType = enum {
+pub const Row = struct {
+    id: u32,
+    username: [username_size]u8,
+    email: [email_size]u8,
+};
+
+pub const InsertStement = struct {
+    row: Row,
+};
+
+pub const StatementTypeTag = enum {
     insert,
     select,
 };
 
+pub const StatementType = union(StatementTypeTag) {
+    insert: InsertStement,
+    select: void,
+};
+
 pub const StatementParseError = error{
     Unrecognized,
+    Invalid,
 };
 
 pub const Statement = struct {
     const Self = @This();
     statement_type: StatementType,
 
-    pub fn parse(buf: []u8) StatementParseError!Self {
+    pub fn parse(buf: []u8, allocator: Allocator) StatementParseError!Self {
         if (std.mem.startsWith(u8, buf, "insert")) {
+            const tokens = std.mem.splitScalar(u8, buf, ' ');
+            const maybe_keyword = tokens.next();
+            if (maybe_keyword) |keyword| {
+                if (!std.mem.eql(u8, keyword, "insert")) {
+                    return error.Invalid;
+                }
+            }
+
+            const row_id: u32 = undefined;
+            const username = try allocator.alloc(u8, 32);
+            const email = try allocator.alloc(u8, 255);
+
             return Self{ .statement_type = .insert };
         } else if (std.mem.startsWith(u8, buf, "select")) {
             return Self{ .statement_type = .select };
